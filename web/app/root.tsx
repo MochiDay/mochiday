c8045ae829697b80d82bd7ecf7258274630a0c86
Xipu Li <lixipu98@gmail.com>
2024-03-25 15:00:41 -0700

    chore: init tailwindcss

--- web/app/root.tsx
+++ web/app/root.tsx
@@ -5,6 +5,12 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import type { LinksFunction } from "@remix-run/cloudflare";
+import stylesheet from "~/tailwind.css?url";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: stylesheet },
+];
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
