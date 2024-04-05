6a1a448325f9303ee26c249205d3d4b6805d2c82
nataliya-a <nkarmarkar@dons.usfca.edu>
2024-04-05 15:34:04 -0700

    feat: pagination

--- web/app/routes/dashboard.jobs.tsx
+++ web/app/routes/dashboard.jobs.tsx
@@ -4,21 +4,40 @@ import { JobsPanel } from "~/components/fresh-jobs/JobsPanel";
 import GeneralDashboardLayout from "~/components/GeneralDashboardLayout";
 import { Job, SideBarType } from "~/types/general";
 
-export const loader: LoaderFunction = async ({ context }) => {
+export const loader: LoaderFunction = async ({ context, request }) => {
   const supabase = context.supabase();
+
+  const pageNumberString = new URL(request.url).searchParams.get("page");
+  const pageNumber = parseInt(pageNumberString ?? "1");
+
+  const totalJobs = await supabase
+    .from("jobs")
+    .select("*", { count: "exact", head: true });
+
+  const pagesRequired = Math.ceil(totalJobs.count! / 20);
+
   const result = await supabase
     .from("jobs")
     .select("*")
     .order("created_at", { ascending: false })
-    .range(0, 19);
-  return json(result.data);
+    .range((pageNumber - 1) * 20, pageNumber * 20 - 1);
+
+  return json({ pageNumber, pagesRequired, jobs: result.data });
 };
 
 export default function DashboardJobs() {
-  const data = useLoaderData() as Job[];
+  const { pageNumber, pagesRequired, jobs } = useLoaderData() as {
+    pageNumber: number;
+    pagesRequired: number;
+    jobs: Job[];
+  };
   return (
     <GeneralDashboardLayout sidebarType={SideBarType.JOBS}>
-      <JobsPanel jobs={data ?? []} />
+      <JobsPanel
+        jobs={jobs ?? []}
+        pageNumber={pageNumber}
+        pagesRequired={pagesRequired}
+      />
     </GeneralDashboardLayout>
   );
 }
